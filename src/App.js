7175aba9724ad5717221bdc9fb187c7e378b6d74
kgodwin88 <kgodwin1988@aol.com>
2018-07-29 11:53:35 -0400

    added function for winning game

--- src/App.js
+++ src/App.js
@@ -5,65 +5,64 @@ import Nav from "./components/Nav";
 import images from "./images.json";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-let newArray =[]
+let newArray = [];
 class App extends Component {
   state = {
     images,
     score: 0,
     topScore: 0,
-    greeting: 'Click an Image to Begin!'
+    greeting: "Click an Image to Begin!"
   };
-  shuffle = array =>{
+  shuffle = array => {
     let counter = array.length;
 
     // While there are elements in the array
     while (counter > 0) {
-        // Pick a random index
-        let index = Math.floor(Math.random() * counter);
+      // Pick a random index
+      let index = Math.floor(Math.random() * counter);
 
-        // Decrease counter by 1
-        counter--;
+      // Decrease counter by 1
+      counter--;
 
-        // And swap the last element with it
-        let temp = array[counter];
-        array[counter] = array[index];
-        array[index] = temp;
+      // And swap the last element with it
+      let temp = array[counter];
+      array[counter] = array[index];
+      array[index] = temp;
     }
 
     return array;
-  }
+  };
   checkMemory = id => {
-    console.log(id)
-    if(!newArray.includes(id)){
-    this.setState({score: this.state.score + 1})
-    this.setState({greeting: "You Guessed Correctly!"})
-    this.shuffle(images)
-    newArray.push(id)
-    if(this.state.score >= this.state.topScore){
-      
-      this.setState({topScore: this.state.score + 1})
-    }
-    if(this.state.topScore = 12){
-      this.setState({greeting:"You won!"})
-      this.setState({score: 0})
-      newArray =[];
-    }
-    }
-    else{
-      this.setState({greeting:"You Guessed Incorrectly"})
-      this.setState({score: 0})
-      newArray =[];
-      this.shuffle(images)
+    console.log(id);
+    if (!newArray.includes(id)) {
+      this.setState({ score: this.state.score + 1 });
+      this.setState({ greeting: "You Guessed Correctly!" });
+      this.shuffle(images);
+      newArray.push(id);
+      if (this.state.score >= this.state.topScore) {
+        this.setState({ topScore: this.state.score + 1 });
+        if (this.state.score === 11) {
+          this.setState({ greeting: "You won!" });
+          this.setState({ score: 0 });
+          newArray = [];
+        }
+      }
+    } else {
+      this.setState({ greeting: "You Guessed Incorrectly" });
+      this.setState({ score: 0 });
+      newArray = [];
+      this.shuffle(images);
     }
   };
 
   render() {
     return (
       <div>
-        <Nav 
-        score = {this.state.score}
-        topScore= {this.state.topScore}
-        greeting ={this.state.greeting} />
+        <Nav
+          score={this.state.score}
+          topScore={this.state.topScore}
+          greeting={this.state.greeting}
+        />
         <Header />
         <Wrapper>
           {this.state.images.map(image => (
